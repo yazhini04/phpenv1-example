70a0801f474e619e4624e32b0079b9081df44118
yazhini04 <ananthirt@gmail.com>
2015-12-15 23:33:44 +0530

    First commit

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+echo "Hello World";
+
+$database = new medoo([
+	'database_type' => 'sqlite',
+	'database_file' => '../storage/database.db'
+]);
+
